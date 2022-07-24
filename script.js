80c9ed1f3ef250d956bdc38381d9a9dd60ecad2a
shannon-paton <79596670+shannon-paton@users.noreply.github.com>
2022-07-24 18:58:43 -0400

    combined if/else statement into more efficient code

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-let homeScore = document.getElementById("home-score");
-let guestScore = document.getElementById("guest-score");
-
 function buttonClicked(number, team) { // get number from button and team
-
-	if ( team === 'home')  {
-		// get current score
-		let currentScore = parseInt(homeScore.innerHTML);
-		// add number to current score
-		currentScore += number;
-		// update score
-		homeScore.innerHTML = currentScore;
-	}
-	else {
-		let currentScore = parseInt(guestScore.innerHTML);
-		currentScore += number;
-		guestScore.innerHTML = currentScore;
-	}
+	scoreBoard = document.getElementById(team)
+	// get current score
+	let currentScore = parseInt(scoreBoard.innerHTML);
+	// add number to current score
+	currentScore += number;
+	// update score
+	scoreBoard.innerHTML = currentScore;
 }
