f0e5fa0db1983cc56ba1afd220c73e8a29a84f6f
shannon-paton <79596670+shannon-paton@users.noreply.github.com>
2022-07-24 18:32:00 -0400

    if else statement for team

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-let homeScore = document.getElementById("home-score")
-let guestScore = document.getElementById("guest-score")
+let homeScore = document.getElementById("home-score");
+let guestScore = document.getElementById("guest-score");
 
-function buttonClicked(number, team) {
-	console.log(number, team)
-
-	// get number from button
-
-	// get team
+function buttonClicked(number, team) { // get number from button and team
 
 	// get current score
+	console.log(number, team)
+
+	if ( team === 'home')  {
+		let currentScore = homeScore;
+		console.log(team)
+	}
+	else {
+		let currentScore = guestScore;
+		console.log(team)
+	}
+
+	//let currentScore = 
 
 	// add number to current score
 
