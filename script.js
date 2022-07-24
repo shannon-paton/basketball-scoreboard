b0ec81decd77759aad57b826f89867650e6faab0
shannon-paton <79596670+shannon-paton@users.noreply.github.com>
2022-07-24 18:24:47 -0400

    added steps in function

--- script.js
+++ script.js
@@ -3,4 +3,15 @@ let guestScore = document.getElementById("guest-score")
 
 function buttonClicked(number, team) {
 	console.log(number, team)
+
+	// get number from button
+
+	// get team
+
+	// get current score
+
+	// add number to current score
+
+	// update score
+
 }
