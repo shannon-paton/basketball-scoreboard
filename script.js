d8a12bf133615186be591238a6d129e5aad0aeb8
shannon-paton <79596670+shannon-paton@users.noreply.github.com>
2022-07-24 18:21:48 -0400

    added function that logs number and team

---
+++ script.js
@@ -0,0 +1,6 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+
+function buttonClicked(number, team) {
+	console.log(number, team)
+}
