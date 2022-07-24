2f9ef944ad1c89e36435211a90ee2c66a7460ff6
shannon-paton <79596670+shannon-paton@users.noreply.github.com>
2022-07-24 18:37:39 -0400

    got current scores in function

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ let guestScore = document.getElementById("guest-score");
 
 function buttonClicked(number, team) { // get number from button and team
 
-	// get current score
 	console.log(number, team)
 
+	// get current score
 	if ( team === 'home')  {
-		let currentScore = homeScore;
-		console.log(team)
+		let currentScore = parseInt(homeScore.innerHTML);
+		console.log("current score is" + " " + currentScore)
 	}
 	else {
-		let currentScore = guestScore;
-		console.log(team)
+		let currentScore = parseInt(guestScore.innerHTML)
+		console.log("current score is" + " " + currentScore)
 	}
 
-	//let currentScore = 
+	
 
 	// add number to current score
 
